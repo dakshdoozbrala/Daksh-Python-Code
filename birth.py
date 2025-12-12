6366a973a651e250b8db80f590ef7e4c80c30e95
dakshdoozbrala <dakshagrawal0903@gmail.com>
2025-12-12 20:22:28 +0530

    Add files via upload

---
+++ birth.py
@@ -0,0 +1,20 @@
+# Inputing Birthdays of people
+
+name1 = input("Enter your name: ")
+birthdate1 = int(input("Enter your birthday: "))
+name2 = input("Enter your name: ")
+birthdate2 = int(input("Enter your birthday: "))
+name3 = input("Enter your name: ")
+birthdate3 = int(input("Enter your birthday: "))
+name4 = input("Enter your name: ")
+birthdate4 = int(input("Enter your birthday: "))
+name5 = input("Enter your name: ")
+birthdate5 = int(input("Enter your birthday: "))
+
+# Print Birthdays
+print("\n\n======= NOW PRINTING RESULT =====")
+print(f"{name1} has birthday on {birthdate1}")
+print(f"{name2} has birthday on {birthdate2}")
+print(f"{name3} has birthday on {birthdate3}")
+print(f"{name4} has birthday on {birthdate4}")
+print(f"{name5} has birthday on {birthdate5}")
