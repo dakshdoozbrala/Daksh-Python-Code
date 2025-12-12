8cc187eff2d02e5dfe2beb68f50ccedb126b056e
dakshdoozbrala <dakshagrawal0903@gmail.com>
2025-12-12 19:37:09 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,37 @@
+#Print a Sentence
+print("Welcome to Python.")
+
+#print a number
+print(4)
+
+#Next Line After Print
+print("Welcome \n")
+
+#Print Multiple Arguements
+print("Welcome",5)
+
+#Print End Sentence
+print("welcome to", end="*")
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+# write  a variable 
+codingal = "Codingal"
+print(codingal)
+
+# Take Input 
+
+input("Enter you name:")
+print("\nwelcome , hello ,\nwelcome to codingal")
+
+# Input Age of a person
+age = int(input("Enter the age of a person:"))
+print(age)
+
+import keyword
+
+print(keyword.kwlist)
