c722286c37a8cf323aadaf5046532c9a54bd46c0
dakshdoozbrala <dakshagrawal0903@gmail.com>
2025-12-15 19:31:40 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,32 @@
+# finding the datatype of diffrent values
+a = 5
+print(" The datatype of a is",type(a))
+
+b = 2.5
+print("The datatype of b is",type(b))
+
+c = "hello"
+print("The datatype of c is",type(c))
+
+d = True
+print("The datatype of d is",type(d))
+
+name = "daksh"
+age = 7
+is_student = True
+weight = 45
+
+print(name)
+print(age)
+print(is_student)
+print (weight)
+
+age = str(age)
+print(age,type(age)) 
+
+weight = int(weight)
+print(weight,type(weight))
+
+a = input("Enter a string:")
+b = a[::-1]
+print(b)
