6aec892223aecab1386f2e7db2dde17fcabde346
dakshdoozbrala <dakshagrawal0903@gmail.com>
2025-12-22 17:45:50 +0530

    Add files via upload

---
+++ season.py
@@ -0,0 +1,16 @@
+temp = int(input("Enter the temperature: "))
+
+if temp >= 30:
+    print("Suitable for wearing light clothes as it is summer")
+else:
+    print("Not suitable for wearing light clothes as it is cold")
+
+# Login section (independent)
+pas1 = input("Enter your password: ")
+pas2 = "Daksh@1234"
+
+if pas1 == pas2:
+    print("Login successful")
+else:
+    print("Login unsuccessful")
+
