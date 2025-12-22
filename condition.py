a4c247c76532d5fcb98d2f79aed7669d4a1a1507
dakshdoozbrala <dakshagrawal0903@gmail.com>
2025-12-22 16:41:57 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,22 @@
+num = int(input("Enter a number:"))
+if num > 0 :
+    print(num," is Positive")
+elif num == 0 :
+    print("The Number is 0")
+else:
+    print(num,"is Negative")
+
+costprice = int(input("Enter the Cost Price:"))
+sellingprice = int(input("Enter the Selling Price"))
+
+if sellingprice > costprice :
+    print("Profit",sellingprice - costprice)
+else:
+    print("Loss",costprice-sellingprice)
+
+a = int(input("Enter a number:"))
+if a > 15 :
+    print("The number is greater than 15")
+else:
+    print("The number is smaller than 15")
+  
