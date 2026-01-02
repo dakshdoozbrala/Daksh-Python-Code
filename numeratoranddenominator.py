e7c075729e460c08caf2c38fa7a274540ecdabfa
dakshdoozbrala <dakshagrawal0903@gmail.com>
2026-01-02 14:51:59 +0530

    Add files via upload

---
+++ numeratoranddenominator.py
@@ -0,0 +1,17 @@
+n = int(input("Enter the value of numerator"))
+m = int(input("Enter the value of denominator"))
+if n%m == 0 :
+    print("It is divisble")
+else:
+    print("It is not divisbble")
+
+mean1 = 38
+w= 36
+r = 56
+total = 40
+sum = mean1*total
+print("The sum is",sum)
+
+a = sum-((w)-(r))
+print(a)
+finalmean  =  a/total
