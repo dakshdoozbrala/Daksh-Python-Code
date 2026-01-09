4f4cb24d8094a8191f9509ad962603ac527476d4
dakshdoozbrala <dakshagrawal0903@gmail.com>
2026-01-09 17:38:56 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,27 @@
+n = int(input("Enter the number whose sum you want to find: "))
+sum= 0
+
+for i in range(1,n+1):
+    sum = sum+i 
+    print(sum)
+
+a = input("enter the name of a person who you want to reverse: ")
+b = ''
+for i in a:
+    b= i+b
+print(b)
+
+x = int(input("Enter the number who you want to reverse: "))
+for i in range(x,0,-1):
+    print(i)
+
+y = 50
+for i in range(y,100+1):
+    print(i)
+
+z = input("Enter a number to reverse it")
+c =""
+for i in z:
+    c = i+c
+w = int(c)
+print(w)
